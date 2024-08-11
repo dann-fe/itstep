28af3db4aa018cf0c2eab640b61fa65e9023541c
Daniel <dan.wf312@gmail.com>
2024-08-11 18:41:23 +0200

    03_functions.py

---
+++ classes/py/8-7-24/users/users.py
@@ -0,0 +1,44 @@
+import os
+
+def get_user_info():
+    username = input("enter your username: ")
+    password = input("enter your password: ")
+    return [username, password]
+
+def user_verification_process():
+    file_in_users = os.path.join("C:/Users/adamk/Desktop/programovani/step_fullstack/classes/py/8-7-24/users" + "/" + username + ".txt")
+    user_exists = os.path.isfile(file_in_users)
+    success = False
+    return [file_in_users, user_exists, success]
+
+print(os.path.isdir("C:/Users/adamk/Desktop/programovani/step_fullstack/classes/py/8-7-24/users"))
+signup_login = input("if you want to login into the system, type 1, if you want to sign up, type 2: ")
+
+if signup_login == "1":
+    username, password = get_user_info()
+
+    file_in_users, user_exists, success = user_verification_process()
+    if user_exists:
+        with open(file_in_users) as file:
+            content = file.read() 
+        if content == password: 
+            success = True
+    if success:
+        print("login successful")
+        success = True
+    else:
+        print("login unsuccessful")
+
+
+elif signup_login == "2":
+    username, password = get_user_info()
+
+    file_in_users, user_exists, success = user_verification_process()
+    if user_exists:
+        print("username taken, please restart the program and choose a different one")
+    else:
+        with open(file_in_users, "w") as file:
+            file.write(password)
+        print("signup successful")
+else:
+    print("invalid input")
